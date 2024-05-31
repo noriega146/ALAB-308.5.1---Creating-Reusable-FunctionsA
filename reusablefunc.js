5bfafa4b2bddb19ec2ba1da22749d0c3b01e105e
noriega146 <129232092+noriega146@users.noreply.github.com>
2024-05-31 13:37:35 -0400

    Update reusablefunc.js

--- reusablefunc.js
+++ reusablefunc.js
@@ -2,14 +2,19 @@
 
 //A. Sum of an array of numbers:
 function sumarray(numbers) {
-    const sum = numbers.reduce((acc, curr) => acc + curr, 0);
-    return sum;
+    const sum = numbers.reduce((acc, curr) => acc + curr, 0); // uses .reduce to sum contents of array
+    return sum;// returns sum of array 
   }
   
   
   //AVG of an array of numbers:
-  function averagearray(numbers){
+function averagearray(numbers){
     if (numbers.lenght === 0) return 0; // avoids dividing by zero
-    const avg = sumarray(numbers) / numbers.lenght;
-    return avg;
+    const avg = sumarray(numbers) / numbers.lenght; // Uses sumarray to get sum and then divide by .lenght of array
+    return avg; // = array sum average
   }
+
+
+  //
+
+
